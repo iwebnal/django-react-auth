6a63772b2406d0a5365f66975ef3195e15bf2f50
iwebnal <iwebnal@gmail.com>
2024-01-20 08:55:19 +0300

    change axios request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,8 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  // baseURL: "http://127.0.0.1:8000"
+  baseURL: "http://nartlang.ru:8000"
 });
 
 function App() {
